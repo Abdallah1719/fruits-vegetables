7684abf475a8f720c68aa5ab8586c5c42975a053
abdallah <abdallahtolba17@gmail.com>
2025-07-12 23:10:22 +0300

    create app file

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FruitsVegetables extends StatelessWidget {
+  const FruitsVegetables({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
